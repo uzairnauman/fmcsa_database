e021db3a3e167ebb63a3f0651ea334c4c280c2c0
Uzair Nauman <134670643+uzairnauman@users.noreply.github.com>
2026-07-10 12:15:33 -0700

    Update upload_motherduck.py

--- upload_motherduck.py
+++ upload_motherduck.py
@@ -1,7 +1,7 @@
 import duckdb
 
 # 1. Define your token and paths
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InV6YWlybmF1bWFuMTdAZ21haWwuY29tIiwibWRSZWdpb24iOiJhd3MtdXMtd2VzdC0yIiwic2Vzc2lvbiI6InV6YWlybmF1bWFuMTcuZ21haWwuY29tIiwicGF0IjoiVmhwOTU2WVM5V25odk1zQUY0OWh4b3JXbUd0UFVlUUp0Sy1zdGVqRmVxUSIsInVzZXJJZCI6IjI1N2MwNjJjLWFmYjEtNDFhNS1iZGE1LTNlOWIxMzJmZDdkNiIsImlzcyI6Im1kX3BhdCIsInJlYWRPbmx5IjpmYWxzZSwidG9rZW5UeXBlIjoicmVhZF93cml0ZSIsImlhdCI6MTc4Mjk1NDA1NH0.Q22UZrNCrzctgWH3HD5FvKYmbHg0Aq8FlkZi12QtH90"
+token = "motherduck token"
 local_db_path = r"E:\Trucking_Dashboard\fmcsa_search.duckdb"
 
 # Note: The 'r' before the string is a "raw string" in Python. 
